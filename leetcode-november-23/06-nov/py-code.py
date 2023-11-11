86edd44edfe591b7a491c84dbdcd774e411f8c85
M H Sohel <149554910+devmhsohel@users.noreply.github.com>
2023-11-11 15:19:47 +0600

    06-nov

---
+++ leetcode-november-23/06-nov/py-code.py
@@ -0,0 +1,23 @@
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.count = 0
+        self.heap = []
+
+    def reserve(self) -> int:
+        if len(self.heap) == 0:
+            self.count = self.count + 1
+            return self.count
+        else :
+            return heapq.heappop(self.heap)
+
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.heap, seatNumber)
+        
+        
+
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
